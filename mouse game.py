6ddd0de5e5b1e8890eb1cc7a08b5973fbfa8753f
MohammadReza Bakhshandeh <mohammadrezabakhshandeh80@gmail.com>
2026-07-16 12:58:13 +0330

    Update mouse game.py

--- mouse game.py
+++ mouse game.py
@@ -25,10 +25,12 @@ font = pygame.font.SysFont('arial', 25)
 small_font = pygame.font.SysFont('arial', 18)
 
 # init colors
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+FFFFF = (30,90,230)
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 200, 0)
 DARK_GREEN = (0, 150, 0)
 BLUE = (0, 0, 255)
 DARK_BLUE = (0, 0, 200)
@@ -43,25 +45,72 @@ def draw_text(text, font_obj, color, x, y):
     screen_rect = screen_text.get_rect(center=(x, y))
     screen.blit(screen_text, screen_rect)
 
-def draw(screen, score):
-    draw_text("Score= " + str(score), font, RED, 70, 20)
+def quit_game():
+    pygame.quit()
+    raise SystemExit
+
+class Player():
+    def __init__(self):
+        self.size = 30
+
+        self.loc_x = SCREEN_WIDTH // 2
+        self.loc_y = SCREEN_HEIGHT // 2
+
+        self.rect = pygame.Rect(self.loc_x, self.loc_y, self.size, self.size)
+
+        self.score = 0
+        
+    def update(self):
+        self.size -= 0.5
+        self.size = max(10, min(self.size, 100))
+
+        self.rect = pygame.Rect(self.loc_x, self.loc_y, self.size, self.size)
+
+        mouse = pygame.mouse.get_pos()
+        self.rect.center = mouse
+
+    def eat(self, food):
+        if self.rect.colliderect(food.rect):
+            pygame.draw.rect(screen, (255,20,20), self.rect)
+            food.update()
+            self.size += 15
+            self.score += 1
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, GREEN, self.rect)
+
+
+class Food:
+    def __init__(self):
+        self.size = 15
+
+        self.loc_x = random.randint(40, SCREEN_WIDTH-40)
+        self.loc_y = random.randint(50, SCREEN_HEIGHT-40)
+
+        self.rect = pygame.Rect(self.loc_x, self.loc_y, self.size, self.size)
+
+    def update(self):
+        self.loc_x = random.randint(40, SCREEN_WIDTH-40)
+        self.loc_y = random.randint(50, SCREEN_HEIGHT-40) 
+
+        self.rect = pygame.Rect(self.loc_x, self.loc_y, self.size, self.size)   
+                                    
+    def draw(self, screen):
+        pygame.draw.rect(screen, FFFFF, self.rect)
+
+
+def draw_ui(player):
+    draw_text("Score= " + str(player.score), font, RED, 70, 20)
     # draw_text("Difficulty= " + str(difficulty), font, RED, SCREEN_WIDTH-100, 20)
     pygame.draw.line(screen, RED, (0,40), (SCREEN_WIDTH,40), 5)
     pygame.draw.line(screen, RED, (0,40), (0, SCREEN_HEIGHT), 5)
     pygame.draw.line(screen, RED, (SCREEN_WIDTH-2,40), (SCREEN_WIDTH-2,SCREEN_HEIGHT), 5)
     pygame.draw.line(screen, RED, (0, SCREEN_HEIGHT-2), (SCREEN_WIDTH,SCREEN_HEIGHT-2), 5)
 
+
 def main():
-    score = 0
-
-    main_rect_size = 30
-    rand_rect_size = 15
-
-    main_x = SCREEN_WIDTH/2
-    main_y = SCREEN_HEIGHT/2
-
-    rand_x = random.randint(40, SCREEN_WIDTH-40)
-    rand_y = random.randint(40, SCREEN_HEIGHT-40)
+    player = Player()
+    food = Food()
 
     run = True
     while run:
@@ -69,39 +118,18 @@ def main():
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                quit_game()
 
+        player.update()
+        player.eat(food= food)
 
-        main_rect = pygame.Rect(main_x, main_y, main_rect_size, main_rect_size)
-        rand_rect = pygame.Rect(rand_x, rand_y, rand_rect_size, rand_rect_size)
-
-        mouse = pygame.mouse.get_pos()
-        main_rect.center = mouse
-
-        pygame.draw.rect(screen, (0,255,0), main_rect)
-        pygame.draw.rect(screen, (30,90,230), rand_rect)
-
-        draw(screen, score)
-
-        main_rect_size -= 0.5
-        if main_rect_size <= 10:
-            main_rect_size = 10
-        elif main_rect_size >= 100:
-            main_rect_size = 100
-
-        if main_rect.colliderect(rand_rect):
-            pygame.draw.rect(screen, (255,20,20), main_rect)
-            rand_x = random.randint(40, SCREEN_WIDTH-40)
-            rand_y = random.randint(40, SCREEN_HEIGHT-40)
-            main_rect_size += 15
-            score += 1
-
-        
+        player.draw(screen= screen)
+        food.draw(screen= screen)
+        draw_ui(player= player)
 
         pygame.display.flip()
 
         clock.tick(30)
 
-    pygame.quit()
 
 main()
